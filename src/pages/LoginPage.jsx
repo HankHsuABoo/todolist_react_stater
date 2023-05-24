bc61d529fdf687bac5a89a9f48b1d989452133f2
HankHsuABoo <hank183753@gmail.com>
2023-05-25 00:31:25 +0800

    add todo api

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -6,8 +6,31 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { login } from '../api/auth';
 
 const LoginPage = () => {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleClick = async () => {
+    if (username.length === 0) {
+      return;
+    }
+    if (password.length === 0) {
+      return;
+    }
+
+    const { success, authToken } = await login({
+      username,
+      password,
+    });
+    if (success) {
+      localStorage.setItem('authToken', authToken);
+    }
+  };
+
   return (
     <AuthContainer>
       <div>
@@ -16,14 +39,27 @@ const LoginPage = () => {
       <h1>登入 Todo</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label={'帳號'}
+          value={username}
+          placeholder={'請輸入帳號'}
+          onChange={(nameInputValue) => setUserName(nameInputValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="password"
+          label={'密碼'}
+          value={password}
+          placeholder={'請輸入密碼'}
+          onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+        />
       </AuthInputContainer>
-      <AuthButton>登入</AuthButton>
-      <AuthLinkText>註冊</AuthLinkText>
+      <AuthButton onClick={handleClick}>登入</AuthButton>
+      <Link to="/signup">
+        <AuthLinkText>註冊</AuthLinkText>
+      </Link>
     </AuthContainer>
   );
 };
